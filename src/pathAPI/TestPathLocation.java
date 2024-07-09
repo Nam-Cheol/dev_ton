35737eb2f47bcc8b47f589d679f1d9b9a4e7a35b
nambanote <namcher9428@gmail.com>
2024-07-09 19:12:55 +0900

    okhttp 라이브러리 추가

---
+++ src/pathAPI/TestPathLocation.java
@@ -0,0 +1,30 @@
+package pathAPI;
+
+
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+public class TestPathLocation {
+
+	public static void main(String[] args) {
+		
+		OkHttpClient client = new OkHttpClient();
+
+//		MediaType mediaType = MediaType.parse("application/json");
+//		RequestBody body = RequestBody.create(mediaType, "{\"startX\":129.06018048,\"startY\":35.15960017,\"angle\":20,\"speed\":30,\"endPoiId\":\"10001\",\"endX\":129.05665309,\"endY\":35.15685041,\"reqCoordType\":\"WGS84GEO\",\"startName\":\"%EC%B6%9C%EB%B0%9C\",\"endName\":\"%EB%8F%84%EC%B0%A9\",\"searchOption\":\"30\",\"resCoordType\":\"WGS84GEO\",\"sort\":\"index\"}");
+//		Request request = new Request.Builder()
+//		  .url("https://apis.openapi.sk.com/tmap/routes/pedestrian?version=1&callback=function")
+//		  .post(body)
+//		  .addHeader("accept", "application/json")
+//		  .addHeader("content-type", "application/json")
+//		  .addHeader("appKey", "YEWVxfrK4j8xTNQZURJ4z1Te4JTZs26v45fgmfn7")
+//		  .build();
+//
+//		Response response = client.newCall(request).execute();
+		
+	}
+	
+}
